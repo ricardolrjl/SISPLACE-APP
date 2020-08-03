b74f151033d2ea094f010718ffc645198c5623e6
Ricardo Jacome <ricardolrjl@gmail.com>
2020-08-03 18:44:00 -0500

    Whatsapp y mensajes

--- app/src/main/java/com/uisrael/sisplaceapp/SitiosActivity.java
+++ app/src/main/java/com/uisrael/sisplaceapp/SitiosActivity.java
@@ -2,7 +2,10 @@ package com.uisrael.sisplaceapp;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.pm.ResolveInfo;
 import android.net.Uri;
@@ -14,12 +17,16 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.snackbar.Snackbar;
+
 import java.util.ArrayList;
 import java.util.List;
 
+import static android.Manifest.permission_group.SMS;
+
 public class SitiosActivity extends AppCompatActivity {
     TextView user;
-    String cedula,nombreUsuario;
+    String cedula, nombreUsuario;
     Bundle datoRecibir;
     Integer idPersonal;
     ImageButton imw;
@@ -36,77 +43,81 @@ public class SitiosActivity extends AppCompatActivity {
         // Activar flecha ir atrás (ir a la Parent Activity declarada en el manifest)
         actionBar.setDisplayHomeAsUpEnabled(true);
 
-        user=findViewById(R.id.tvUsuario);
+        user = findViewById(R.id.tvUsuario);
 
-        datoRecibir=getIntent().getExtras();
-        cedula=datoRecibir.getString("usuario");
-        nombreUsuario=datoRecibir.getString("nombre");
-        idPersonal=datoRecibir.getInt("idpersonal");
+        datoRecibir = getIntent().getExtras();
+        cedula = datoRecibir.getString("usuario");
+        nombreUsuario = datoRecibir.getString("nombre");
+        idPersonal = datoRecibir.getInt("idpersonal");
         user.setText(nombreUsuario);
 
-        imw=findViewById(R.id.imgBtnWhatsapp);
+        imw = findViewById(R.id.imgBtnWhatsapp);
+
+        //if(ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.SEND_SMS))
     }
 
-    public void irFacebook(View v){
+    public void irFacebook(View v) {
         String facebookId = "fb://page/235467633241041";
         String urlPage = "https://www.facebook.com/agentesdequito/";
 
-       try {
+        try {
             startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(facebookId)));
         } catch (Exception e) {
-            Toast.makeText(getApplicationContext(),"Aplicación no instalada",Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), "Aplicación no instalada", Toast.LENGTH_SHORT).show();
             //Abre url de pagina.
             startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(urlPage)));
         }
     }
 
-    public void irInicio(View v){
-        Intent intentEnvio= new Intent(SitiosActivity.this, InicioActivity.class);
-        intentEnvio.putExtra("usuario",cedula);
-        intentEnvio.putExtra("nombre",nombreUsuario);
-        intentEnvio.putExtra("idpersonal",idPersonal);
+    public void irInicio(View v) {
+        Intent intentEnvio = new Intent(SitiosActivity.this, InicioActivity.class);
+        intentEnvio.putExtra("usuario", cedula);
+        intentEnvio.putExtra("nombre", nombreUsuario);
+        intentEnvio.putExtra("idpersonal", idPersonal);
         startActivity(intentEnvio);
-   //     Toast.makeText(getApplicationContext(),"Ir al Inicio",Toast.LENGTH_SHORT).show();
+        //     Toast.makeText(getApplicationContext(),"Ir al Inicio",Toast.LENGTH_SHORT).show();
     }
 
-    public void irYoutube(View v){
+    public void irYoutube(View v) {
         String YoutubeId = "yt://MN0nU_su5u17xqHnhs2JSQ";
         String urlPage = "https://www.youtube.com/channel/UCUguziC608WwBWWSkcJZX1Q";
 
         try {
             startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(YoutubeId)));
         } catch (Exception e) {
-            Toast.makeText(getApplicationContext(),"Aplicación no instalada",Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), "Aplicación no instalada", Toast.LENGTH_SHORT).show();
             //Abre url de pagina.
             startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(urlPage)));
         }
     }
-    public void irTwiter(View v){
+
+    public void irTwiter(View v) {
         String TwiterId = "tw://page/235467633241041";
         String urlPage = "https://twitter.com/agentesdequito?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor";
 
         try {
             startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(TwiterId)));
         } catch (Exception e) {
-            Toast.makeText(getApplicationContext(),"Aplicación no instalada",Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), "Aplicación no instalada", Toast.LENGTH_SHORT).show();
             //Abre url de pagina.
             startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(urlPage)));
         }
     }
-    public void irPagina(View v){
+
+    public void irPagina(View v) {
 
         String urlPage = "https://cuerpodeagentesdecontrolquito.gob.ec/";
 
         try {
             startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(urlPage)));
         } catch (Exception e) {
-            Toast.makeText(getApplicationContext(),"Aplicación no instalada",Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), "Aplicación no instalada", Toast.LENGTH_SHORT).show();
             //Abre url de pagina.
             startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(urlPage)));
         }
     }
 
-    public void irWhatsapp(View v){
+    public void irWhatsapp(View v) {
 /*
         try {
             Uri uri = Uri.parse("smsto:"+"+593987342976");
@@ -118,21 +129,12 @@ public class SitiosActivity extends AppCompatActivity {
         }
 
 */
-try {
-    Uri sms_uri = Uri.parse("smsto:+" + "593992850775");
-    Intent sms_intent = new Intent(Intent.ACTION_SENDTO, sms_uri);
-    sms_intent.putExtra("sms_body", "Mi nombre es "+nombreUsuario+" ,necesito ayuda con ");
-    startActivity(sms_intent);
-}
-catch (Exception e){
-    Uri sms_uri = Uri.parse("smsto:+" + "593992850775");
-    Intent sms_intent = new Intent(Intent.ACTION_SENDTO, sms_uri);
-    sms_intent.putExtra("sms_body", "Mi nombre es "+nombreUsuario+" ,necesito ayuda con ");
-    startActivity(sms_intent);
-}
 
 
-
+        Uri sms_uri = Uri.parse("smsto:+" + "593992850775");
+        Intent sms_intent = new Intent(Intent.ACTION_SENDTO, sms_uri);
+        sms_intent.putExtra("sms_body", "Mi nombre es " + nombreUsuario + " ,necesito ayuda con ");
+        startActivity(sms_intent);
 
 
     }
